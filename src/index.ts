9ec96858aad117da413b42541d3e7b7e4889c90c
Joshua Beatty <joshuatb6@gmail.com>
2022-07-02 23:49:37 -0600

    adding rimraf to build

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import console from 'node-color-log';
 import express from "express";
 import apiRouter from "./api/router";
 import appRouter from "./app/router";
-const app = express();
-const port = 8080; // default port to listen
 
-// define a route handler for the default home page
+//Set up server and default port
+const app = express();
+const port = process.env.PORT || 8080; 
+
+// import route handlers for the api and for the main app
 app.use( "/api/", apiRouter);
 app.use( "/", appRouter);
 
-// start the Express server
+// start the Server
 app.listen( port, () => {
     console.info( `server started at http://localhost:${ port }` );
     
